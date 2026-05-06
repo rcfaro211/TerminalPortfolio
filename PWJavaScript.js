82a343cbd52e0d6f0f0e55966d8edc6acdd47039
Ryan Cristofaro <ryan_cristofaro@students.pittsford.monroe.edu>
2026-05-06 10:05:09 -0400

    fixed output not displaying issue

--- PWJavaScript.js
+++ PWJavaScript.js
@@ -6,10 +6,7 @@ const PROJECTLIST = [
 	`-1 ${padding}Terminal Style Portfolio (This Project)`,
 	`-2 ${padding}CLI Toolkit (WIP)`,
 ];
-const PROJECTLISTSTR = [
-	"Terminal Style Portfolio",
-	"CLI Toolkit (WIP)"
-]
+const PROJECTLISTSTR = ["Terminal Style Portfolio", "CLI Toolkit (WIP)"];
 const project_List = PROJECTLIST.join("\n");
 const project_help = `Usage: Projects \n-(1-${PROJECTLIST.length})${padding}Opens a project depending on the number\n      ${padding}(see project list with "Projects -l")\n\n-H    ${padding}Displays this menu\n\n-L    ${padding}Displays a list of all my projects\n`;
 //}
@@ -21,20 +18,14 @@ const COMMANDLIST = [
 	`LS      ${padding}Lists all available directories`,
 	`PROJECTS${padding}Show all of my projects`,
 ];
-const DIRECTORYLIST = [
-	"PROJECTS",
-	"ABOUT.TXT",
-	"CREDITS.EXE",
-	"EXPERIENCE.TXT",
-	"GUI.EXE",
-];
+const DIRECTORYLIST = ["ABOUT.TXT", "CREDITS.EXE", "EXPERIENCE.TXT", "GUI.EXE"];
 const directory_List = DIRECTORYLIST.join("\n");
 const commands_List = COMMANDLIST.join("\n");
-const outputField = document.getElementById("CmdOutputField");
+const outputField = document.getElementById("cmdOutputField");
 let prefix = "PF C:\\>";
 document.getElementById("prefix").textContent = `${prefix}`;
 //}
-// let mode = "normal";
+
 
 cmdField.addEventListener("submit", function (event) {
 	event.preventDefault();
@@ -43,27 +34,17 @@ cmdField.addEventListener("submit", function (event) {
 	const cmdInput = input.value.toUpperCase().trim();
 	const indexMatching = cmdInput.match(/^PROJECTS -(\d+)$/);
 
-	// if (mode === "projectOpen") {
-	// 	const allowed = ["1", "2"];
-	// 	const cmdOutput = document.createElement("div");
-	// 	cmdOutput.id = "cmdOutputs";
-	// 	cmdOutput.textContent = `${prefix} ${cmdInput}`;
-	// 	outputField.appendChild(cmdOutput);
-
-	// 	if (allowed.includes(cmdInput))
-	// }
-
 	if (cmdInput === "") {
 		return;
 	}
 	//{ Help menu command
 	if (cmdInput === "HELP" || cmdInput === "H") {
 		const cmdOutput = document.createElement("div");
-		cmdOutput.id = "cmdOutputs";
+		cmdOutput.className = "cmdOutputs";
 		cmdOutput.textContent = `${prefix} ${cmdInput}`;
 		outputField.appendChild(cmdOutput);
 		const helpMenu = document.createElement("div");
-		helpMenu.id = "cmdOutputs";
+		helpMenu.className = "cmdOutputs";
 		helpMenu.textContent = `${commands_List}`;
 		outputField.appendChild(helpMenu);
 		input.value = "";
@@ -76,11 +57,11 @@ cmdField.addEventListener("submit", function (event) {
 		const echoList = cmdInput.split(" ");
 		if (echoList[0] === "ECHO") {
 			const cmdOutput = document.createElement("div");
-			cmdOutput.id = "cmdOutputs";
+			cmdOutput.className = "cmdOutputs";
 			cmdOutput.textContent = `${prefix} ${cmdInput}`;
 			outputField.appendChild(cmdOutput);
 			const echoOutput = document.createElement("div");
-			echoOutput.id = "cmdOutputs";
+			echoOutput.className = "cmdOutputs";
 			const echoText = cmdInput.replace(/echo /i, "");
 			echoOutput.textContent = echoText;
 			outputField.appendChild(echoOutput);
@@ -89,11 +70,11 @@ cmdField.addEventListener("submit", function (event) {
 			return;
 		} else {
 			const cmdOutput = document.createElement("div");
-			cmdOutput.id = "cmdOutputs";
+			cmdOutput.className = "cmdOutputs";
 			cmdOutput.textContent = `${prefix} ${cmdInput}`;
 			outputField.appendChild(cmdOutput);
 			const syntaxErrorMsg = document.createElement("div");
-			syntaxErrorMsg.id = "cmdOutputs";
+			syntaxErrorMsg.className = "cmdOutputs";
 			syntaxErrorMsg.textContent =
 				"Syntax Error. Correct Syntax: 'echo [Text]'";
 			outputField.appendChild(syntaxErrorMsg);
@@ -105,7 +86,7 @@ cmdField.addEventListener("submit", function (event) {
 	//}
 	//{ Cls command
 	if (cmdInput === "CLS") {
-		document.querySelectorAll("#cmdOutputs").forEach((el) => el.remove());
+		document.querySelectorAll(".cmdOutputs").forEach(el => el.remove());
 		input.value = "";
 		input.focus();
 		return;
@@ -114,11 +95,11 @@ cmdField.addEventListener("submit", function (event) {
 	//{ Git Command
 	if (cmdInput === "GIT") {
 		const cmdOutput = document.createElement("div");
-		cmdOutput.id = "cmdOutputs";
+		cmdOutput.className = "cmdOutputs";
 		cmdOutput.textContent = `${prefix} ${cmdInput}`;
 		outputField.appendChild(cmdOutput);
 		const loadingMsg = document.createElement("div");
-		loadingMsg.id = "cmdOutputs";
+		loadingMsg.className = "cmdOutputs";
 		loadingMsg.textContent = "Opening Github Page...";
 		outputField.appendChild(loadingMsg);
 		window.open("https://github.com/rcfaro211", "_blank");
@@ -130,12 +111,12 @@ cmdField.addEventListener("submit", function (event) {
 	//{ LS command
 	if (cmdInput === "LS") {
 		const cmdOutput = document.createElement("div");
-		cmdOutput.id = "cmdOutputs";
+		cmdOutput.className = "cmdOutputs";
 		cmdOutput.textContent = `${prefix} ${cmdInput}`;
 		outputField.appendChild(cmdOutput);
 		const directorys = document.createElement("div");
 		directorys.textContent = directory_List;
-		directorys.id = "cmdOutputs";
+		directorys.className = "cmdOutputs";
 		outputField.appendChild(directorys);
 		input.value = "";
 		input.focus();
@@ -145,11 +126,11 @@ cmdField.addEventListener("submit", function (event) {
 	//{ Project command
 	if (cmdInput === "PROJECTS -H" || cmdInput === "PROJECTS") {
 		const cmdOutput = document.createElement("div");
-		cmdOutput.id = "cmdOutputs";
+		cmdOutput.className = "cmdOutputs";
 		cmdOutput.textContent = `${prefix} ${cmdInput}`;
 		outputField.appendChild(cmdOutput);
 		const projectHelp = document.createElement("div");
-		projectHelp.id = "cmdOutputs";
+		projectHelp.className = "cmdOutputs";
 		projectHelp.textContent = project_help;
 		outputField.appendChild(projectHelp);
 		input.value = "";
@@ -158,11 +139,11 @@ cmdField.addEventListener("submit", function (event) {
 	}
 	if (cmdInput === "PROJECTS -L") {
 		const cmdOutput = document.createElement("div");
-		cmdOutput.id = "cmdOutputs";
+		cmdOutput.className = "cmdOutputs";
 		cmdOutput.textContent = `${prefix} ${cmdInput}`;
 		outputField.appendChild(cmdOutput);
 		const projectList = document.createElement("div");
-		projectList.id = "cmdOutputs";
+		projectList.className = "cmdOutputs";
 		projectList.textContent = `${project_List}`;
 		outputField.appendChild(projectList);
 		input.value = "";
@@ -170,20 +151,23 @@ cmdField.addEventListener("submit", function (event) {
 		return;
 	}
 	if (indexMatching) {
-		const index = Number((indexMatching[1] -= 1));
+		const index = Number(indexMatching[1]) - 1;
 		if (index >= 0 && index < PROJECTLIST.length) {
 			if (index === 0) {
 				const cmdOutput = document.createElement("div");
-				cmdOutput.id = "cmdOutputs";
+				cmdOutput.className = "cmdOutputs";
 				cmdOutput.textContent = `${prefix} ${cmdInput}`;
 				outputField.appendChild(cmdOutput);
 				const project1 = document.createElement("div");
-				project1.id = "cmdOutputs";
+				project1.className = "cmdOutputs";
 				project1.textContent = `Opening ${PROJECTLISTSTR[0]}`;
 				outputField.appendChild(project1);
-				window.open("https://github.com/rcfaro211/TerminalPortfolio", "_blank")
-				input.value = ""
-				input.focus()
+				window.open(
+					"https://github.com/rcfaro211/TerminalPortfolio",
+					"_blank",
+				);
+				input.value = "";
+				input.focus();
 				return;
 			} else if (index === 1) {
 				console.log("Project 2");
@@ -192,7 +176,7 @@ cmdField.addEventListener("submit", function (event) {
 	}
 	//}
 	const cmdOutput = document.createElement("div");
-	cmdOutput.id = "cmdOutputs";
+	cmdOutput.className = "cmdOutputs";
 	cmdOutput.textContent = `${prefix} ${cmdInput}`;
 	outputField.appendChild(cmdOutput);
 	input.value = "";
